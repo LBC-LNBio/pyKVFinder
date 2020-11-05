0ffbb28a89758419de52b163d3deba61467f4927
jvsguerra <jvsguerra@gmail.com>
2020-11-05 14:07:59 -0300

    Add functions to read vdw data in different formats

--- pyKVFinder/modules/utils.py
+++ pyKVFinder/modules/utils.py
@@ -1,4 +1,5 @@
-import os, sys, site
+import os
+import sys
 import numpy as np
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -8,5 +9,41 @@ print(here)
 def read_pdb(fn: str):
     pass
 
-def read_vdw():
-    pass
+
+def read_vdw_dat(fn: str):
+    """
+    Read van der Waals radii from .dat format
+    """
+    vdw = {}
+    
+    with open(fn, 'r') as f:
+        # Read line with data only (ignore empty lines)
+        lines = [line.replace(' ', '') for line in f.read().splitlines() if line.replace('\t\t', '')]
+        for line in lines:
+            if line:
+                if line.startswith('>'):
+                    res = line.replace('>', '').replace('\t\t', '')
+                    vdw[res] = {}
+                else:
+                    atom, radius = line.split('\t\t')
+                    vdw[res][atom] = float(radius)
+    
+    return vdw
+
+
+def read_vdw_json(fn: str):
+    """
+    Read van der Waals radii from .json format
+    """
+    import json
+    with open(fn, "r") as f:
+        return json.load(f)
+
+
+def read_vdw_toml(fn: str):
+    """
+    Read van der Waals radii from .toml format
+    """
+    import toml
+    with open(fn, "r") as f:
+        return toml.load(f)
