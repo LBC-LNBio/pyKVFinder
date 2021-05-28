642628586d21bc3fc0b8fef57e64b63961a3114f
jvsguerra <jvsguerra@gmail.com>
2021-05-28 15:26:51 -0300

    Update docstring and code format

--- pyKVFinder/argparser.py
+++ pyKVFinder/argparser.py
@@ -1,20 +1,33 @@
 import os
 import argparse
+from typing import Any
 from . import __name__, __version__
 
 __all__ = ["argparser"]
 
 
-def _check_hydropathy_options(x: str) -> str:
-    """
-    Checks if x is a acceptable option for hydropathy argument
+def _check_hydropathy_options(x: Any) -> str:
+    """Checks if x is a acceptable option for hydropathy argument.
+
     Parameters
     ----------
-        x (str): option to be checked
+    x : Any
+        Option to be checked.
 
     Returns
     -------
-        x (str): accepted option
+    x : str
+        Valid option.
+
+    Raises
+    ------
+    argparse.ArgumentTypeError
+        Invalid TOML-formatted file: {x} (define a TOML-formatted
+        file with hydrophobicity values for standard residues).
+    argparse.ArgumentTypeError
+        Invalid choice: '{x}' (choose from 'EisenbergWeiss', 'HessaHeijne',
+        'KyteDoolittle', 'MoonFleming', 'WimleyWhite', 'ZhaoLondon' or define a
+        TOML-formatted file with hydrophobicity values for standard residues).
     """
     if x in [
         "EisenbergWeiss",
@@ -31,73 +44,92 @@ def _check_hydropathy_options(x: str) -> str:
         try:
             toml.load(x)
         except toml.decoder.TomlDecodeError:
-            msg = f"invalid TOML-formatted file: {x} (define a TOML-formatted file with hydrophobicity values for standard residues)"
+            msg = f"Invalid TOML-formatted file: {x} (define a TOML-formatted \
+file with hydrophobicity values for standard residues)."
             raise (argparse.ArgumentTypeError(msg))
         return x
     else:
-        msg = f"invalid choice: {x} (choose from 'EisenbergWeiss', 'HessaHeijne', 'KyteDoolittle', 'MoonFleming', 'WimleyWhite', 'ZhaoLondon' or define a TOML-formatted file with hydrophobicity values for standard residues)"
+        msg = f"Invalid choice: '{x}' (choose from 'EisenbergWeiss', 'HessaHeijne', \
+'KyteDoolittle', 'MoonFleming', 'WimleyWhite', 'ZhaoLondon' or define a \
+TOML-formatted file with hydrophobicity values for standard residues)."
         raise (argparse.ArgumentTypeError(msg))
 
 
-def _positive_float(x: float) -> float:
-    """
-    Checks if x is a float
+def _positive_float(x: Any) -> float:
+    """Checks if x is a float.
 
     Parameters
     ----------
-        x (float): variable to be checked
+    x : Any
+        Variable to be checked.
 
     Returns
     -------
-        x (float): float variable
+    x : float
+        A valid float.
+
+    Raises
+    ------
+    argparse.ArgumentTypeError
+        `x` is not a floating-point literal.
+    argparse.ArgumentTypeError
+        `x` is not a positive floating-point.
     """
     try:
         x = float(x)
     except ValueError:
-        msg = "%r not a floating-point literal" % (x,)
+        msg = "%r is not a floating-point literal." % (x,)
         raise (argparse.ArgumentTypeError(msg))
 
     if x < 0.0:
-        msg = "%r not a positive floating-point" % (x,)
+        msg = "%r is not a positive floating-point." % (x,)
         raise (argparse.ArgumentTypeError(msg))
     return x
 
 
-def _restricted_step_size(x: float) -> float:
-    """
-    Checks: 0.0 < x <= 2.0
+def _restricted_step_size(x: Any) -> float:
+    """Checks if x is in range (0.0, 2.0].
 
     Parameters
     ----------
-        x (float): grid spacing (A)
+    x : Any
+        Grid spacing (A).
 
     Returns
     -------
-        x (float): grid spacing (A) between 0.0 and 2.0
+    x : float
+        Grid spacing (A) between (0.0, 2.0].
+
+    Raises
+    ------
+    argparse.ArgumentTypeError
+        `x` is not a floating-point literal.
+    argparse.ArgumentTypeError
+        `x` is not in range (0.0, 2.0].
     """
     try:
         x = float(x)
     except ValueError:
-        msg = "%r not a floating-point literal" % (x,)
+        msg = "%r is not a floating-point literal." % (x,)
         raise (argparse.ArgumentTypeError(msg))
 
     if not 0.0 < x <= 2.0:
-        msg = "%r not in range (0.0, 2.0]" % (x,)
+        msg = "%r is not in range (0.0, 2.0]." % (x,)
         raise (argparse.ArgumentTypeError(msg))
     return x
 
 
 def argparser() -> argparse.ArgumentParser:
-    """
-    Defines pyKVFinder argument parser
+    """Defines pyKVFinder argument parser.
 
     Parameters
     ----------
-        None
+    None
 
     Returns
     -------
-        parser (argparse.ArgumentParser): pyKVFinder argument parser
+    parser : argparse.ArgumentParser
+        pyKVFinder argument parser
     """
     # Overrides method in HelpFormatter
     class CapitalisedHelpFormatter(argparse.HelpFormatter):
@@ -121,7 +153,10 @@ def argparser() -> argparse.ArgumentParser:
 
     # Positional arguments
     parser.add_argument(
-        "pdb", metavar="<.pdb>", type=os.path.abspath, help="Path to a target PDB file."
+        "pdb",
+        metavar="<.pdb>",
+        type=os.path.abspath,
+        help="Path to a target PDB file.",
     )
 
     # Optional arguments
@@ -139,7 +174,11 @@ def argparser() -> argparse.ArgumentParser:
         help="Show pyKVFinder version number and exit.",
     )
     parser.add_argument(
-        "-b", "--base_name", metavar="<str>", type=str, help="Prefix to output files."
+        "-b",
+        "--base_name",
+        metavar="<str>",
+        type=str,
+        help="Prefix to output files.",
     )
     parser.add_argument(
         "-O",
@@ -154,7 +193,8 @@ def argparser() -> argparse.ArgumentParser:
         metavar="<int>",
         type=int,
         default=os.cpu_count() - 1,
-        help="Number of threads to apply in parallel routines (default: %(default)s)",
+        help="Number of threads to apply in parallel routines \
+(default: %(default)s)",
     )
 
     # Create argument group
@@ -200,7 +240,7 @@ def argparser() -> argparse.ArgumentParser:
         metavar="<float>",
         default=5.0,
         type=_positive_float,
-        help="Cavities volume filter (\u212B\u00b3). (default: %(default).1lf)",
+        help="Cavities volume filter (\u212B\u00b3). (default: %(default).1f)",
     )
     parameters.add_argument(
         "-R",
@@ -208,7 +248,8 @@ def argparser() -> argparse.ArgumentParser:
         metavar="<float>",
         default=2.4,
         type=_positive_float,
-        help="Length to be removed from the cavity-bulk frontier (\u212B). (default: %(default).1lf)",
+        help="Length to be removed from the cavity-bulk frontier (\u212B). \
+(default: %(default).1lf)",
     )
     parameters.add_argument(
         "-S",
@@ -217,13 +258,16 @@ def argparser() -> argparse.ArgumentParser:
         type=str,
         default="SES",
         choices=["SES", "SAS"],
-        help="Surface representation. Options: %(choices)s. SES specifies solvent excluded surface. SAS specifies solvent accessible surface. (default: %(default)s)",
+        help="Surface representation. Options: %(choices)s. \
+SES specifies solvent excluded surface. SAS specifies solvent accessible \
+surface. (default: %(default)s)",
     )
     parameters.add_argument(
         "--ignore_backbone",
         action="store_true",
         default=False,
-        help="Ignore backbone contacts to cavity when defining interface residues. (default: %(default)s)",
+        help="Ignore backbone contacts to cavity when defining interface \
+residues. (default: %(default)s)",
     )
 
     # Extra modes
@@ -232,23 +276,31 @@ def argparser() -> argparse.ArgumentParser:
         "-D",
         "--depth",
         action="store_true",
-        default=None,
-        help="Depth characterization of the detected cavities. Map depth in B-factor in the cavity PDB file and maximum and average depth of the detected cavities. (default: %(default)s)",
+        default=False,
+        help="Depth characterization of the detected cavities. \
+Map depth in B-factor in the cavity PDB file and maximum and average depth of \
+the detected cavities. (default: %(default)s)",
     )
     extra_modes.add_argument(
         "--plot_frequencies",
         action="store_true",
-        default=None,
-        help="Plot histograms of calculated frequencies of the detected cavities in a PDF file. (default: %(default)s)",
+        default=False,
+        help="Plot histograms of calculated frequencies of the detected \
+cavities in a PDF file. (default: %(default)s)",
     )
     extra_modes.add_argument(
         "--hydropathy",
         type=_check_hydropathy_options,
-        metavar="{EisenbergWeiss, HessaHeijne, KyteDoolittle, MoonFleming, WimleyWhite, ZhaoLondon, <.toml>}",
+        metavar="{EisenbergWeiss, HessaHeijne, KyteDoolittle, MoonFleming, \
+WimleyWhite, ZhaoLondon, <.toml>}",
         nargs="?",
         const="EisenbergWeiss",
-        default=None,
-        help="Hydropathy characterization of the detected cavities. Map hydrophobicity scale values as B-factor at surface points of detected cavities. Hydrophobicity scales options: %(metavar)s. A custom hydrophobicity scale can be defined via a TOML-formatted file. (default: %(default)s) (constant: %(const)s)",
+        default=False,
+        help="Hydropathy characterization of the detected cavities. Map \
+hydrophobicity scale values as B-factor at surface points of detected \
+cavities. Hydrophobicity scales options: %(metavar)s. A custom hydrophobicity \
+scale can be defined via a TOML-formatted file. (default: %(default)s) \
+(constant: %(const)s)",
     )
 
     # Create argument group
@@ -260,7 +312,11 @@ def argparser() -> argparse.ArgumentParser:
         "--box",
         metavar="<.toml>",
         type=os.path.abspath,
-        help='A path to TOML-formatted file with box parameters. Adjust the 3D grid based on a list of residues (["resnum", "chain"]) and a padding or a set of four vertices (p1: origin, p2: X-axis max, p3: Y-axis max, p4: Z-axis max) with xyz coordinates ([x, y, z]). Define the custom 3D grid with only one of this box parameters.',
+        help='A path to TOML-formatted file with box parameters. Adjust the \
+3D grid based on a list of residues (["resnum", "chain", "resname"]) and a \
+padding or a set of four vertices (p1: origin, p2: X-axis max, p3: Y-axis \
+max, p4: Z-axis max) with xyz coordinates ([x, y, z]). Define the custom 3D \
+grid with only one of this box parameters.',
     )
 
     # Create argument group
@@ -272,14 +328,16 @@ def argparser() -> argparse.ArgumentParser:
         "--ligand",
         metavar="<.pdb>",
         type=os.path.abspath,
-        help="Path to a ligand PDB file to limit the cavities within a radius (ligand_cutoff) around it.",
+        help="Path to a ligand PDB file to limit the cavities within a radius \
+(ligand_cutoff) around it.",
     )
     ligand_adjustment.add_argument(
         "--ligand_cutoff",
         metavar="<float>",
         default=5.0,
         type=_positive_float,
-        help="Radius value to limit a space around the defined ligand. (default: %(default).1lf)",
+        help="Radius value to limit a space around the defined ligand. \
+(default: %(default).1lf)",
     )
 
     return parser
