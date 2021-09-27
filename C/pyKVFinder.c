dddd8861ef1a93c36501979a2d87baaac6d23e09
jvsguerra <jvsguerra@gmail.com>
2021-09-27 15:36:43 -0300

    Update C extension

--- C/pyKVFinder.c
+++ C/pyKVFinder.c
@@ -786,7 +786,6 @@ void DFS(int *grid, int nx, int ny, int nz, int i, int j, int k, int tag)
 
         if (!big)
         {
-#pragma omp taskloop shared(i, j, k, nx, ny, nz, tag, grid), private(x, y, z)
             for (x = i - 1; x <= i + 1; x++)
                 for (y = j - 1; y <= j + 1; y++)
                     for (z = k - 1; z <= k + 1; z++)
