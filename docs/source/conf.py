f13db6ded83c0c251d0c9c4f8ad62e908af427df
jvsguerra <jvsguerra@gmail.com>
2022-08-16 16:17:27 -0300

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = "2021, LBC-LNBio"
 author = "LBC-LNBio"
 
 # The short X.Y version
-version = "0.3"
+version = "0.4"
 # The full version, including alpha/beta/rc tags
-release = "0.3.2"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -68,7 +68,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
