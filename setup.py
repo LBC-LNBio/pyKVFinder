6277e226e1b347ffa2e88230d66444b4a68fdd99
jvsguerra <jvsguerra@gmail.com>
2023-02-01 20:51:50 -0300

    Update workflow

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import Extension, setup
 
+
+class get_numpy_include(object):
+    def __str__(self):
+        import numpy
+
+        return numpy.get_include()
+
+
 setup(
     ext_modules=[
         Extension(
             name="_pyKVFinder",
             sources=["C/pyKVFinder.i", "C/pyKVFinder.c"],
-            include_dirs=["C"],
+            include_dirs=[get_numpy_include(), "C"],
             extra_compile_args=["-fopenmp", "-Ofast", "-lm"],
             extra_link_args=["-lgomp"],
         ),
