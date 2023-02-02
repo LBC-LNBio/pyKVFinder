0e3c0c25988551f5aff489639331cd96e95d4b90
jvsguerra <jvsguerra@gmail.com>
2023-02-02 16:45:02 -0300

    Update testing workflow

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -1,29 +1,70 @@
+import argparse
 import os
-import subprocess
+import toml
 import unittest
 
 import pyKVFinder
-
 
 DATADIR = os.path.join(os.path.dirname(pyKVFinder.__file__), "data")
 
 
 class TestCLI(unittest.TestCase):
-    def test_standard_mode(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_standard_mode(self, args):
         # Run pyKVFinder CLI with standard mode
         # $ pyKVFinder <.pdb>
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
 
-    def test_box_mode(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=os.path.join(DATADIR, "tests", "custom-box.toml"),
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_custom_box_mode(self, args):
         # Run pyKVFinder CLI with box mode
         # $ pyKVFinder <.pdb> -B <.toml>
         # - custom_box.toml
@@ -32,149 +73,310 @@ class TestCLI(unittest.TestCase):
         # >>> p2 = [x2, y2, z2]
         # >>> p3 = [x3, y3, z3]
         # >>> p4 = [x4, y4, z4]
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "-B",
-                os.path.join(DATADIR, "tests", "custom-box.toml"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=os.path.join(DATADIR, "tests", "residues-box.toml"),
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_residues_box_mode(self, args):
         # - residues_box.toml
         # >>> [box]
         # >>> residues = [ ["resnum", "chain", "resname",], ["resnum", "chain",
         # ... "resname"], ]
         # >>> padding =  3.5
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "-B",
-                os.path.join(DATADIR, "tests", "residues-box.toml"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
 
-    def test_lig_mode(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=os.path.join(DATADIR, "tests", "ADN.pdb"),
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_lig_mode(self, args):
         # Run pyKVFinder CLI with ligand mode
         # $ pyKVFinder <.pdb> -L <.pdb>
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "-L",
-                os.path.join(DATADIR, "tests", "ADN.pdb"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
 
-    def test_hydropathy_mode(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy="EisenbergWeiss",
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_hydropathy_mode(self, args):
         # Run pyKVFinder CLI with hydropathy mode
         # $ pyKVFinder <.pdb> --hydropathy
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "--hydropathy",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
 
-    def test_depth_mode(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=True,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_depth_mode(self, args):
         # Run pyKVFinder CLI with depth mode
         # $ pyKVFinder <.pdb> --depth
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "--depth",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 0)
+        self.assertEqual(pyKVFinder.main.cli(), 0)
 
-    def test_bad_args(self):
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input="non-existing.pdb",
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_non_existing_receptor(self, args):
         # non-existing <.pdb>
         # $ pyKVFinder non-existing.pdb
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                "non-existing.pdb",
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 1)  # FileNotFoundError
+        self.assertRaises(FileNotFoundError, pyKVFinder.main.cli)  # FileNotFoundError
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out="string",
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_bad_float(self, args):
         # bad float (step, probe_in, probe_out, removal_distance, volume_cutoff, ligand_cutoff)
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-o",
-                "string",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 2)  # argparse.ArgumentTypeError
+        # $ pyKVFinder -o string
+        self.assertRaises(TypeError, pyKVFinder.main.cli)  # TypeError
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="A",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_bad_surface(self, args):
         # bad surface
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-S",
-                "A",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 2)  # argparse.ArgumentTypeError
-        # non-existing ligand <.pdb>
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "-O",
-                os.path.join(DATADIR, "tests", "output"),
-                "-L",
-                "non-existing.pdb",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 1)  # FileNotFoundError
-        # bad hydropathy file (not a TOML file)
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "--hydropathy",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 2)  # argparse.ArgumentTypeError
-        # non-existing dictionary file
-        result = subprocess.run(
-            [
-                "pyKVFinder",
-                os.path.join(DATADIR, "tests", "1FMO.pdb"),
-                "--hydropathy",
-                "non-existing-dictionary.dat",
-            ],
-            capture_output=True,
-        )
-        self.assertEqual(result.returncode, 2)  # argparse.ArgumentTypeError
+        # $ pyKVFinder -S A
+        # self.assertEqual(pyKVFinder.main.cli(), 2)  # argparse.ArgumentTypeError
+        self.assertRaises(ValueError, pyKVFinder.main.cli)  # ValueError
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=False,
+            box=None,
+            ligand="non-existing.pdb",
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_non_existing_ligand(self, args):
+        # bad surface
+        # $ pyKVFinder -L non-existing.pdb
+        self.assertRaises(FileNotFoundError, pyKVFinder.main.cli)  # FileNotFoundError
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy="non-existing-dictionary.dat",
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_non_existing_hydropathy_file(self, args):
+        # bad surface
+        # $ pyKVFinder --hydopathy non-existing-dictionary.dat
+        self.assertRaises(FileNotFoundError, pyKVFinder.main.cli)  # FileNotFoundError
+
+    @unittest.mock.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            input=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            verbose=False,
+            base_name=None,
+            output_directory=os.path.join(DATADIR, "tests", "output"),
+            model=None,
+            nthreads=11,
+            dictionary=os.path.join(DATADIR, "vdw.dat"),
+            step=0.6,
+            probe_in=1.4,
+            probe_out=4.0,
+            volume_cutoff=5.0,
+            removal_distance=2.4,
+            surface="SES",
+            ignore_backbone=False,
+            depth=False,
+            plot_frequencies=False,
+            hydropathy=os.path.join(DATADIR, "tests", "1FMO.pdb"),
+            box=None,
+            ligand=None,
+            ligand_cutoff=5.0,
+        ),
+    )
+    def test_bad_hydropathy_file(self, args):
+        # bad surface
+        # $ pyKVFinder --hydopathy <.pdb>
+        self.assertRaises(
+            toml.decoder.TomlDecodeError, pyKVFinder.main.cli
+        )  # toml.decoder.TomlDecodeError
